102d31010b07aa55c2766bd5e58be391f2265759
shahwiruddin10 <149791182+shahwiruddin10@users.noreply.github.com>
2025-10-28 11:55:26 +0700

    footer.blade.php

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; SB-Admin 2 Website 2025</span>
                     </div>
                 </div>
             </footer>
